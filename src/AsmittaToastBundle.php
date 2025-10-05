cb3139a4712310350781feab94f7cfd7711692f0
Asmitta <tiwabrayan@gmail.com>
2025-10-05 18:54:57 +0200

    Initialize repo

---
+++ src/AsmittaToastBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Asmitta\ToastBundle;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+/**
+ * @author Brayan Tiwa <tiwabrayan@gmail.com>
+ * @copyright 2025 Brayan Tiwa 
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ */
+class AsmittaToastBundle extends AbstractBundle implements CompilerPassInterface {}
